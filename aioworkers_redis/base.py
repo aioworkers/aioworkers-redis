a3110ea26e2440114572aa40b30d18426ce4b448
Alexander Malev <yttrium@somedev.ru>
2017-04-29 17:32:29 +0300

    overload decode/encode in pool

--- aioworkers_redis/base.py
+++ aioworkers_redis/base.py
@@ -6,3 +6,11 @@ class RedisPool(FormattedEntity):
     def pool(self):
         key_pool = self.config.get('pool', 'redis_pool')
         return self.context.app[key_pool].get()
+
+    def decode(self, b):
+        if b is not None:
+            return super().decode(b)
+
+    def encode(self, b):
+        if b is not None:
+            return super().encode(b)
