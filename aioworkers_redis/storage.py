aac1ef1752ce7dc4374913848a0d6915fe1f9ac4
Alexander Malev <yttrium@somedev.ru>
2017-04-29 17:32:52 +0300

    FieldStorage

--- aioworkers_redis/storage.py
+++ aioworkers_redis/storage.py
@@ -1,4 +1,4 @@
-from aioworkers.storage.base import AbstractListedStorage
+from aioworkers.storage.base import AbstractListedStorage, FieldStorageMixin
 
 from aioworkers_redis.base import RedisPool
 
@@ -39,3 +39,47 @@ class RedisStorage(RedisPool, AbstractListedStorage):
             value = await conn.get(raw_key)
         if value is not None:
             return self.decode(value)
+
+
+class FieldStorage(FieldStorageMixin, RedisStorage):
+
+    async def set(self, key, value, *, field=None, fields=None):
+        raw_key = self.raw_key(key)
+        to_del = []
+        async with self.pool as conn:
+            if field:
+                if value is None:
+                    to_del.append(field)
+                else:
+                    return await conn.hset(raw_key, field, self.encode(value))
+            elif value is None:
+                return await conn.delete(raw_key)
+            else:
+                pairs = []
+                for f in fields or value:
+                    v = value[f]
+                    if v is None:
+                        to_del.append(f)
+                    else:
+                        pairs.extend((f, self.encode(v)))
+                if pairs:
+                    await conn.hmset(raw_key, *pairs)
+            if to_del:
+                await conn.hdel(raw_key, *to_del)
+
+    async def get(self, key, *, field=None, fields=None):
+        raw_key = self.raw_key(key)
+        async with self.pool as conn:
+            if field:
+                return self.decode(await conn.hget(raw_key, field))
+            elif fields:
+                v = await conn.hmget(raw_key, *fields)
+                m = self.model()
+                for f, v in zip(fields, v):
+                    m[f] = self.decode(v)
+            else:
+                a = await conn.hgetall(raw_key)
+                m = self.model()
+                for f, v in a.items():
+                    m[f.decode()] = self.decode(v)
+            return m
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -2,7 +2,8 @@ import uuid
 
 import aioredis
 from aioworkers.core.config import MergeDict
-from aioworkers_redis.storage import RedisStorage
+from aioworkers.core.context import Context
+from aioworkers_redis.storage import RedisStorage, FieldStorage
 
 
 async def test_storage(loop):
@@ -22,3 +23,29 @@ async def test_storage(loop):
     assert ['g'] == await q.list()
     await q.set('g', None)
     assert not await q.length()
+
+
+async def test_field_storage(loop):
+    key = '6'
+    data = {'f': 3, 'g': 4, 'h': 5}
+    fields = ['f', 'g']
+    config = MergeDict(
+        name='',
+        prefix=str(uuid.uuid4()),
+        format='json',
+    )
+    context = Context({}, loop=loop)
+    context['app.redis_pool'] = await aioredis.create_pool(
+        ('localhost', 6379), loop=loop)
+    storage = FieldStorage(config, context=context, loop=loop)
+    await storage.init()
+    await storage.set(key, data)
+    assert data == await storage.get(key)
+    assert 5 == await storage.get(key, field='h')
+    await storage.set(key, 6, field='h')
+    await storage.set(key, None, field='f')
+    assert {'f': None, 'g': 4} == await storage.get(key, fields=fields)
+    await storage.set(key, {'g': None}, fields=['g'])
+    await storage.set(key, {'z': 1, 'y': 6}, fields=['z'])
+    assert {'h': 6, 'z': 1} == await storage.get(key)
+    await storage.set(key, None)
