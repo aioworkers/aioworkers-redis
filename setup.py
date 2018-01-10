dc748925bcfc254d6e5f827661fe199299a4d6a5
Alexander Malev <yttrium@somedev.ru>
2018-01-10 04:08:40 +0300

    0.4.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ test_requirements = [
 
 setup(
     name='aioworkers_redis',
-    version='0.4.0',
+    version='0.4.1',
     description="Module for working with redis",
     author="Alexander Malev",
     author_email='yttrium@somedev.ru',
